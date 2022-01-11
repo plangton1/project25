d0c20d1796cf59661cb11ce6e38970d46cc472d4
bossatapol18 <67771778+bossatapol18@users.noreply.github.com>
2022-01-11 09:22:51 +0700

    11/1/65 9.22 แก้หน้า home ไป

--- dashboard/index.php
+++ dashboard/index.php
@@ -12,7 +12,7 @@
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
               <h4  class="font-mirt"><a href="">สมอ คืออะไร</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <p>งานด้านการมาตรฐานในประเทศไทยเริ่มใน พ.ศ. 2446 (ร.ศ.122) เมื่อมีการตราพระราชบัญญัติเงินตรารัตนโกสินทร์ศก 122 ขึ้น ซึ่งเป็นพระราชบัญญัติที่มีกฎเกณฑ์ ในการทำเหรียญกษาปณ์ตามหลักวิทยาศาสตร์ ในการนี้กระทรวงพระคลังมหาสมบัติได้จัดตั้งกองแยกธาตุ เพื่อวิเคราะห์ควบคุมคุณภาพของเนื้อเงินที่ใช้ทำเหรียญกษาปณ์ให้เป็นไปตามมาตรฐานที่กำหนด</p>
             </div>
           </div>
 
@@ -20,7 +20,7 @@
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
               <h4  class="font-mirt"><a href="">ขั้นตอนการยื่นเอกสาร</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <p>ปัจจุบันสินค้าที่ สมอ. กำหนดเป็นมาตรฐานปัจจุบันมีอยู่หลายพัน เรื่อง ครอบคลุมสินค้าที่เราใช้ อยู่ในชีวิตประจำวันหลายๆ ประเภท สำนักงานมาตรฐานผลิตภัณฑ์อุตสาหกรรม(สมอ.) สังกัดกระทรวงอุตสาหกรรม ซึ่งเป็นสถาบันมาตรฐาน แห่งชาติ มีหน้าที่ดำเนินการกำหนดมาตรฐาน สามารถทำได้ตามขั้นตอนด้านล่างดังรูป</p>
             </div>
           </div>
 
@@ -28,7 +28,7 @@
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
               <h4  class="font-mirt"><a href="">การขอใบรับรอง</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <p>ผลิตภัณฑ์ที่แสดงเครื่องหมายมอก.ได้นั้น ต้องได้รับการตรวจสอบจากสมอ.แล้วว่ามีคุณภาพเป็นไปตาม ที่กำหนดถ้าผ่าน สมอ.จะออกใบอนุญาตให้ผู้ผลิตแสดงเครื่องหมาย มอก.ที่ผลิตภัณฑ์ของตนได้ หลังจากนั้นสมอ.ก็จะมีการติดตามผลโดยการตรวจสอบระบบควบคุมคุณภาพของโรงงานและสุ่มตัวอย่างผลิตภัณฑ์ ทั้งจากโรงงานสถานที่นำเข้าและสถานที่จำหน่ายมาตรวจสอบอย่างสม่ำเสมอเพื่อให้แน่ใจได้ว่าผลิตภัณฑ์ที่แสดง เครื่องหมายมอก. จะมีคุณภาพตามมาตรฐานและโรงงานยังสามารถรักษาคุณภาพไว้ได้ตามที่กำหนด</p>
             </div>
           </div>
 
@@ -37,7 +37,7 @@
         <div class="col-lg-8">
           <div class="categories">
             <div class="main-button-red">
-            <img src="https://www.tisi.go.th/data/contact/info/info_6.jpg" width="700px" height="1000px" class="ml-img">
+            <img src="https://www.tisi.go.th/data/stories/license/pdf/PIL_th.jpg" width="700px" height="1000px" class="ml-img">
             </div>
           </div>
         </div>
